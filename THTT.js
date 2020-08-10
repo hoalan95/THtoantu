414f8321dd622f584fc375dc844647696e9afee6
hoalan95 <hoatvimamura@gmail.com>
2020-08-10 10:17:38 +0700

    add index1 THTT.js

---
+++ THTT.js
@@ -0,0 +1,9 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt("enter the width");
+inputheight = prompt("enter the height");
+let width = parseInt(inputwidth);
+console.log(width);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write("the area is: " + area);
